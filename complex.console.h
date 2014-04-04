43c77bae92cf4edfb9d1e542858c1cb40a2018ad
Sedovla96 <Egolior@mail.ru>
2014-04-04 16:37:27 +0300

    prototypes2

---
+++ complex.console.h
@@ -0,0 +1,8 @@
+#ifndef COMPLEX_CONSOLE_H
+#define COMPLEX_CONSOLE_H
+#include "functions.h"
+
+void inputNumber( Complex &number );
+void showNumber( Complex number );
+
+#endif // COMPLEX_CONSOLE_H
